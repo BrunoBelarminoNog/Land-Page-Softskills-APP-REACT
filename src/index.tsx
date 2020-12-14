e007ecc39b64ea8af5e75670aa92f922448a5a9a
Bruno Belarmino <brunobelarm.nog@gmail.com>
2020-12-13 23:06:49 -0300

    Adicionado loader inicial

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router } from 'react-router-dom'
 import App from './App';
-import history from './services/history'
-
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router history={history}>
-      <App />
-    </Router>
-  </React.StrictMode>,
+      <App />,
   document.getElementById('root')
 );
